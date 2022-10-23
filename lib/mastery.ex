4eb63abcde3392f759ef3a044e3a81b5534e8c3f
Ondrej Prazak <o.prazak5@seznam.cz>
2022-10-23 13:08:56 +0200

    Initial commit

---
+++ lib/mastery.ex
@@ -0,0 +1,49 @@
+defmodule Mastery do
+  alias Mastery.Boundary.{QuizSession, QuizManager, Proctor}
+  alias Mastery.Boundary.{TemplateValidator, QuizValidator}
+  alias Mastery.Core.Quiz
+
+  @persistence_fn Application.get_env(:mastery, :persistence_fn)
+
+  def build_quiz(fields) do
+    with :ok <- QuizValidator.errors(fields),
+      :ok <-GenServer.call(QuizManager, {:build_quiz, fields}),
+      do: :ok, else: (error -> error)
+  end
+
+  def add_template(title, fields) do
+    with :ok <- TemplateValidator.errors(fields),
+      :ok <- GenServer.call(QuizManager, {:add_template, title, fields}),
+      do: :ok, else: (error -> error)
+  end
+
+  def take_quiz(title, email) do
+    with %Quiz{}=quiz <- QuizManager.lookup_quiz_by_title(title),
+      {:ok, _} <- GenServer.start_link(QuizSession, {quiz, email})
+    do
+      {title, email}
+    else
+      error -> error
+    end
+  end
+
+  def select_question(session) do
+    GenServer.call(session, :select_question)
+  end
+
+  def answer_question(name, answer, persistence_fn \\ @persistence_fn) do
+    QuizSession.answer_question(name, answer, persistence_fn)
+  end
+
+  def schedule_quiz(quiz, templates, start_at, end_at, notify_pid \\ nil) do
+    with :ok <- QuizValidator.errors(quiz),
+      true <- Enum.all?(templates, &(:ok == TemplateValidator.errors(&1))),
+      :ok <- Proctor.schedule_quiz(
+        quiz,
+        templates,
+        start_at,
+        end_at,
+        notify_pid),
+    do: :ok, else: (error -> error)
+  end
+end
---
+++ lib/mastery/boundary/quiz_session.ex
@@ -0,0 +1,98 @@
+defmodule Mastery.Boundary.QuizSession do
+  alias Mastery.Core.{Quiz, Response}
+  use GenServer
+
+  def child_spec({quiz, email}) do
+    %{
+      id: {__MODULE__, {quiz.title, email}},
+      start: { __MODULE__, :start_link, [{quiz, email}]},
+      restart: :temporary
+    }
+  end
+
+  def start_link({quiz, email}) do
+    GenServer.start_link(
+      __MODULE__,
+      {quiz, email},
+      name: via({quiz.title, email})
+    )
+  end
+
+  def active_sessions_for(quiz_title) do
+    Mastery.Supervisor.QuizSession
+      |> DynamicSupervisor.which_children
+      |> Enum.filter(&child_pid?/1)
+      |> Enum.flat_map(&active_sessions(&1, quiz_title))
+  end
+
+  defp child_pid?({:undefined, pid, :worker, [__MODULE__]})
+    when is_pid(pid) do
+      true
+    end
+  end
+  defp child_pid?(_child), do: false
+
+  defp active_sessions({:undefined, pid, :worker, [__MODULE__]}, title) do
+    Mastery.Registry.QuizSession
+      |> Registry.keys(pid)
+      |> Enum.filter(fn {quiz_title, _email} ->
+        quiz_title == title
+      end)
+  end
+
+  def end_sessions(names) do
+    Enum.each(names, fn name -> GenServer.stop(via(name)) end)
+  end
+
+  def take_quiz(quiz, email) do
+    DynamicSupervisor.start_child(
+      Mastery.Supervisor.QuizSession,
+      {__MODULE__, {quiz, email}}
+    )
+  end
+
+  def via({_title, _email}=name) do
+    {
+      :via,
+      Registry,
+      {Mastery.Registry.QuizSession, name}
+    }
+  end
+
+  def init({quiz, email}) do
+    {:ok, {quiz, email}}
+  end
+
+  def handle_call(:select_question, _from, {quiz, email}) do
+    quiz = Quiz.select_question(quiz)
+    {:reply, quiz.current_question.asked, {quiz, email}}
+  end
+
+  def handle_call({:answer_question, answer, fun}, _from, {quiz, email}) do
+    fun = fun || fn r, f -> f.(r) end
+    response = Response.new(quiz, email, answer)
+    fun.(response, fn r ->
+      quiz
+        |> Quiz.answer_question(r)
+        |> Quiz.select_question
+    end)
+      |> maybe_finish(email)
+  end
+
+  defp maybe_finish(nil, _email), do: {:stop, :normal, :finished, nil}
+  defp maybe_finish(quiz, email) do
+    {
+      :reply,
+      {quiz.current_question.asked, quiz.last_response.correct},
+      {quiz, email}
+    }
+  end
+
+  def select_question(name) do
+    GenServer.call(via(name), :select_question)
+  end
+
+  def answer_question(name, answer, persistence_fn) do
+    GenServer.call(via(name), {:answer_question, answer, persistence_fn})
+  end
+end
